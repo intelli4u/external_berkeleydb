a91b9145bbe289b7308ff223f0144bb80b5d5f7e
Gerrit Pusher <pusher@localhost>
2010-11-08 23:54:24 +0800

    Import db v5.0.32.NC

--- build_vxworks/test_micro/b_latch.c
+++ build_vxworks/test_micro/b_latch.c
@@ -1,7 +1,7 @@
 /*-
  * See the file LICENSE for redistribution information.
  *
- * Copyright (c) 2009 Oracle.  All rights reserved.
+ * Copyright (c) 2009, 2010 Oracle and/or its affiliates.  All rights reserved.
  *
  * $Id$
  */
@@ -176,7 +176,7 @@ b_latch(argc, argv)
 	if (nthreads > 0) {
 		for (i = 0; i < nthreads; i++)
 			DB_BENCH_ASSERT(pthread_create(&threads[i].id,
-			    NULL, latch_threadmain, &threads[i]) == 0);
+			    NULL, b_latch_latch_threadmain, &threads[i]) == 0);
 		for (i = 0; i < nthreads; i++) {
 			ret = pthread_join(threads[i].id, &status);
 			DB_BENCH_ASSERT(ret == 0);
